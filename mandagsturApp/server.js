9f92e92632dbe519e104eb7d5ffc787ea4568817
eirikurdal <eirikurdal@gmail.com>
2019-03-10 00:10:23 +0100

    fjernet route til users

--- mandagsturApp/server.js
+++ mandagsturApp/server.js
@@ -4,8 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./js/dbconnect').db; //database
 
-const users = require('./js/users.js');
-app.use('/innafor/users/', users);
+//const users = require('./js/users.js');
+//app.use('/innafor/users/', users);
 
 const port = (process.env.PORT || 3000);
 
