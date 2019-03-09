d868eb7289358639683130858c8032fa3863014a
eirikurdal <eirikurdal@gmail.com>
2019-03-09 16:55:19 +0100

    Added env and gitignore

--- mandagsturApp/server.js
+++ mandagsturApp/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./js/dbconnect').db; //database
 const app = express();
 const port = (process.env.PORT || 3000);
 
 
 //const users = require('./js/users.js');
-
+// app.use('/innafor/users/', users);
 
 
 app.set('port', port);
@@ -14,9 +15,12 @@ app.use(bodyParser.json());
 
 
 
-// app.use('/innafor/users/', users);
 
-
+app.use(function (req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    next(); //go to the specified route
+});
 
 
 
