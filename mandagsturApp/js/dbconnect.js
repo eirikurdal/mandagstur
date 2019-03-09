f6321ba59b7e18ff0164ebe7ca614f215d854db0
eirikurdal <eirikurdal@gmail.com>
2019-03-10 00:07:20 +0100

    added public folder and index.html

--- mandagsturApp/js/dbconnect.js
+++ mandagsturApp/js/dbconnect.js
@@ -1,4 +1,5 @@
 const pgp = require('pg-promise')();
+
 //db connect string
 const db = pgp(process.env.DATABASE_URL || 'postgres://gyqpcejnvdoldq:3c8c6a220b67a08e0e387b87285a44302c0d641aca40c8f6bb8badbc49809fc7@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d8765ihigeqthb?ssl=true');
 
--- mandagsturApp/server.js
+++ mandagsturApp/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
+const app = express();
+
 const bodyParser = require('body-parser');
 const db = require('./js/dbconnect').db; //database
-const app = express();
+
+const users = require('./js/users.js');
+app.use('/innafor/users/', users);
+
 const port = (process.env.PORT || 3000);
-
-
-//const users = require('./js/users.js');
-// app.use('/innafor/users/', users);
 
 
 app.set('port', port);
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-
-
 
 app.use(function (req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
